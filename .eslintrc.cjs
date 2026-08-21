e66c5a25ce542d0bb2f01a27a52cc827a23ea53c
Knud Kjølhede Petersen <39059047+knudkp@users.noreply.github.com>
2026-08-21 15:08:59 +0000

    Upload af alle filer til Cloudflare Meet

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+npx wrangler login --copy/** @type {import('eslint').Linter.Config} */
+module.exports = {
+	extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node'],
+	ignorePatterns: ['/public/noise/*'],
+	rules: {
+		'@typescript-eslint/no-extra-semi': ['off'],
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{
+				// vars: "all",
+				varsIgnorePattern: '^_',
+				// args: "after-used",
+				argsIgnorePattern: '^_',
+			},
+		],
+	},
+}
