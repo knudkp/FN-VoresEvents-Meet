65998413885c878be9af733acc386905894cd02c
Knud Kjølhede Petersen <39059047+knudkp@users.noreply.github.com>
2026-08-22 22:36:01 +0200

    navneændring

--- app/root.tsx
+++ app/root.tsx
@@ -76,7 +76,7 @@ export const loader = async ({ request, context }: LoaderFunctionArgs) => {
 
 export const meta: MetaFunction = () => [
 	{
-		title: 'Orange Meets',
+		title: 'Fleksjobber Netværket Møde',
 	},
 ]
 
@@ -128,8 +128,8 @@ const Document: FC<{ children?: ReactNode }> = ({ children }) => {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta name="apple-mobile-web-app-title" content="Orange Meets" />
-				<meta name="application-name" content="Orange Meets" />
+				<meta name="apple-mobile-web-app-title" content="Fleksjobber Netværket Møde" />
+				<meta name="application-name" content="Fkejsjobber Netværket Møde" />
 				<meta name="msapplication-TileColor" content="#ffffff" />
 				<meta
 					name="theme-color"
