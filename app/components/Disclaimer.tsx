80e0ebbc9aeafc6a498499744f133ec89e20afeb
Knud Kjølhede Petersen <39059047+knudkp@users.noreply.github.com>
2026-08-22 18:42:19 +0200

    Revise Disclaimer component content and styles
    Updated the Disclaimer component to reflect new content and styling.

--- app/components/Disclaimer.tsx
+++ app/components/Disclaimer.tsx
@@ -7,25 +7,8 @@ interface DisclaimerProps {
 
 export const Disclaimer: FC<DisclaimerProps> = ({ className }) => {
 	return (
-		<p
-			className={cn(
-				'text-xs text-zinc-400 dark:text-zinc-500 max-w-prose',
-				className
-			)}
-		>
-			Orange Meets is a demo application built using{' '}
-			<a className="underline" href="https://developers.cloudflare.com/calls/">
-				Cloudflare Calls
-			</a>
-			. To build your own WebRTC application using Cloudflare Calls, get started
-			in the{' '}
-			<a
-				className="underline"
-				href="https://dash.cloudflare.com/?to=/:account/calls"
-			>
-				Cloudflare Dashboard
-			</a>
-			.
+		<p className={cn('text-xs text-zinc-400', className)}>
+			© 2026 - Vores Events
 		</p>
 	)
 }
